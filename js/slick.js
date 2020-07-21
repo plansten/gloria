0391ec688c9703faa439ed3e703c49b21517635b
plansten <sten2@ukr.net>
2020-07-21 10:17:46 +0300

    210720

---
+++ js/slick.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+$('.slider').slick({
+    centerMode: true, 
+    centerPadding: '0px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
